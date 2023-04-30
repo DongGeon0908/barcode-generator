126c3598d089d4292572ca0c2cbb2029870edc75
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2023-04-30 12:06:48 +0900

    fix: barcode generator

--- src/main/kotlin/com/goofy/qrcode/controller/QrRestController.kt
+++ src/main/kotlin/com/goofy/qrcode/controller/QrRestController.kt
@@ -2,17 +2,17 @@ package com.goofy.qrcode.controller
 
 import com.goofy.qrcode.extension.wrapFileResponse
 import com.goofy.qrcode.service.QrService
-import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/v1/qr-code")
+@RequestMapping("/api/v1/qr-codes")
 class QrRestController(
     private val qrService: QrService
 ) {
-    @GetMapping
+    @PostMapping
     fun download(
         @RequestParam filename: String
     ) = qrService.downloadQrCode().wrapFileResponse(filename)
--- src/main/kotlin/com/goofy/qrcode/service/QrService.kt
+++ src/main/kotlin/com/goofy/qrcode/service/QrService.kt
@@ -1,20 +1,22 @@
 package com.goofy.qrcode.service
 
-import com.goofy.qrcode.util.QrCodeGenerator
-import com.goofy.qrcode.util.QrCodeGenerator.Companion.generateQrCode
+import com.goofy.qrcode.util.BarcodeGenerator
+import com.goofy.qrcode.util.BarcodeGenerator.Companion.generateBarCode
+import com.google.zxing.BarcodeFormat
 import org.springframework.stereotype.Service
 
 @Service
 class QrService {
     fun downloadQrCode(): ByteArray {
-        return generateQrCode(
-            QrCodeGenerator.QrCodeModel(
+        return generateBarCode(
+            BarcodeGenerator.BarcodeModel(
+                type = BarcodeFormat.QR_CODE,
                 content = "www.naver.com",
                 width = 400,
                 height = 500,
                 onColor = 0xFF000000.toInt(),
                 offColor = 0xFFFFFFFF.toInt(),
-                fileFormat = QrCodeGenerator.FileFormat.PNG
+                fileFormat = BarcodeGenerator.FileFormat.PNG
             )
         )
     }
